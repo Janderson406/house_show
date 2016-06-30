b10468c846813f9e8e9433023d04287fe5013500
Jon Anderson <jondanderson@gmail.com>
2016-06-29 21:49:14 -0600

    Paperclip/AWS setup for images, avatars working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # This method associates the attribute ":avatar" with a file attachment
+  has_attached_file :avatar, styles: { medium: "300x300>", micro: '35x35#', thumb: "100x100#" }, default_url: "/images/:style/missing.png"
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+
+
+
 end
