82cd4120498cca30642d6b6ba5f16cb9c9e5d710
Jon Anderson <jondanderson@gmail.com>
2016-07-06 19:38:47 -0600

    Create Event crud and views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :user
-  belongs_to :category  
+  belongs_to :category
+
+  enum status: [:pending, :accepted, :denied]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706042124) do
+ActiveRecord::Schema.define(version: 20160706044718) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160706042124) do
     t.integer  "user_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "category_id"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id"
